3b4d97094e82903542a5cb433290494697aef061
Tracey King <6742945+TraceyK14@users.noreply.github.com>
2025-03-02 19:17:20 -0500

    valentines script

---
+++ valentines.js
@@ -0,0 +1,14 @@
+<script>let ll;const nn=document.createElement("style");nn.textContent=`:root {--color-grey: #f3f3ee;--color-red: #ce0b0b;--color-white: #ffffff;--color-brand-brown: #8e5e01;--color-brand-pink: #ffccdb;--color-brand-cream: #ffd6ab;--color-brand-green: #3ec498;}[href="/valentines-special"],.vst .site-wide-fulfillment__edit,.vst .mini-cart {color: var(--color-red) !important;}.vst,.vst .mini-cart,.vst .w-block-background,.vst .nav-mobile.w-block,.vst .search-bar div > div{background: var(--color-brand-pink) !important;}.vst .header__icons a[type='button'],.vst .site-wide-fulfillment__edit,.vst .nav-mobile a[type='button']{background: var(--color-red) !important;}.vst .site-wide-fulfillment__edit,.vst .site-wide-fulfillment__edit{color: var(--color-white) !important;}[href="/valentines-special"]::after{border-bottom-color: var(--color-red) !important;}.vst .nav-mobile .sign-in-link > button > span {color: black;}`;document.head.appendChild(nn);
+window.addEventListener('load', function(){let hh = window.location.href;let bd = document.querySelector("body");let ol;const cc={childList: true,subtree: true};
+function im() {ll.src="https://lh3.googleusercontent.com/d/1PhZgyhR_Fb6y4649gB3DXhfoKSrj9DkN=w100?authuser=1/view";};
+if (window.location.pathname === '/valentines-special') {document.body.classList.add('vst');};const oo=new MutationObserver(function () {
+  if (!ll && document.querySelector(".w-sitelogo img")){ll=document.querySelector(".w-sitelogo img");ol=ll.src;window.location.pathname==='/valentines-special'&&im()};
+if (hh != window.location.href) {hh=window.location.href;
+if (window.location.pathname==='/valentines-special') {
+document.body.classList.add('vst');if(ll){im();};
+} else {document.body.classList.remove('vst');if(ll) {ll.src = ol;};};window.requestAnimationFrame(cm);};});
+function cm() {
+let tt = document.querySelector("body");
+if (tt != bd) {bd = tt;oo.observe(bd, cc);};};oo.observe(bd, cc);});
+
+</script>
